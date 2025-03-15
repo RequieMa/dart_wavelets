5f97bfe863d3e8cf9046cadaff7b0d2feacca7c4
RequieMa <mzo_7071@qq.com>
2025-03-15 14:30:25 +0800

    Init Project

---
+++ example/dart_wavelets_example.dart
@@ -0,0 +1,6 @@
+import 'package:dart_wavelets/dart_wavelets.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ test/dart_wavelets_test.dart
@@ -0,0 +1,16 @@
+import 'package:dart_wavelets/dart_wavelets.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('A group of tests', () {
+    final awesome = Awesome();
+
+    setUp(() {
+      // Additional setup goes here.
+    });
+
+    test('First Test', () {
+      expect(awesome.isAwesome, isTrue);
+    });
+  });
+}
